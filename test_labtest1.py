dcb0680d413b855af88bc1eb79d074ec6c5e666f
TheinMwaeThanTha <theinmwae.21@ichat.sp.edu.sg>
2022-11-28 18:04:00 +0800

    LabTest

---
+++ test_labtest1.py
@@ -0,0 +1,22 @@
+import labtest1
+import csv
+
+
+def test_calc_average_expenses():
+
+    print("")
+
+def test_calc_total_expenses():
+    print("")
+
+def test_is_price_range_valid():
+    print("");
+
+def test_get_items_by_price_range():
+    print("")
+
+def test_sort_by_items():
+    labtest1.sort_by_items()
+    print()
+
+
